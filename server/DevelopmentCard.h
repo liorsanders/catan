87adaf6f72d427293a506f7a03366a4930636c26
liorsanders <sanderslior@gmail.com>
2022-02-20 15:04:09 +0200

    implemented development enum

---
+++ server/DevelopmentCard.h
@@ -0,0 +1,12 @@
+#ifndef DEVELOPMENT_CARD_H
+#define DEVELOPMENT_CARD_H
+
+typedef enum {
+    knight,
+    road_building,
+    year_of_plenty,
+    monopoly,
+    victory_point
+}DevelopmentCard;
+
+#endif
