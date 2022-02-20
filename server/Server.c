39ed9fb2aa51318f11f4f189d10d47b4ec093f2c
liorsanders <sanderslior@gmail.com>
2022-02-20 17:38:57 +0200

    implemented Hexagon functions and tested

--- server/Server.c
+++ server/Server.c
@@ -3,33 +3,21 @@
 #include "Player.h"
 #include "Resource.h"
 #include "SpecialCard.h"
+#include "Hexagon.h"
+
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main() {
-    Resource* resources = (Resource*)malloc(sizeof(Resource)*3);
-    resources[0] = wheat;
-    resources[1] = sheep;
-    resources[2] = ore;
-    printf("created\n");
-    Player* redPlayer = createPlayer(red, resources, 3);
-    Player* bluePlayer = createPlayer(blue, resources, 3);
-    printf("adding\n");
-    addAvailablePiece(redPlayer, settlement_piece);
-    discardAvailablePiece(redPlayer, settlement_piece);
-    addResource(redPlayer, wheat);
-    addResource(redPlayer, wood);
-    discardResource(redPlayer, wood);
-    addSpecialCard(bluePlayer, longest_road);
-    discardSpecialCard(bluePlayer, longest_road);
-    addDevelopmentCard(bluePlayer, knight);
-    discardDevelopmentCard(bluePlayer, knight);
-
-    free(resources);
-    destructPlayer(redPlayer);
-    destructPlayer(bluePlayer);
-    free(redPlayer);
-    free(bluePlayer);
-    printf("destroyed\n");
+    Placement place;
+    place.board_placement = settlement_piece;
+    place.color = blue;
+    Hexagon* hex = createHexagon(wood, 6);
+    printf("created\nadding...\n");
+    addPlacement(hex, 0, place);
+    place.board_placement = city_piece;
+    addPlacement(hex, 0, place);
+    printf("done\n");
+    free(hex);
 }
