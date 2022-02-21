99b525752902163b39c1cb66e1e43bf4b583f13b
liorsanders <sanderslior@gmail.com>
2022-02-21 12:17:18 +0200

    finished constants

--- server/Constants.h
+++ server/Constants.h
@@ -11,23 +11,23 @@
 #define CLIENT_OPEN_KNIGHT "007" //%2d%d, hex index, color to take from
 #define CLIENT_OPEN_VICTORY_POINT "008" 
 #define CLIENT_BUY_SETTLEMENT "009" //%2d%d, hex index, corner index
-#define CLIENT_OPEN_YEAR_OF_PLENTY "010" 
-#define CLIENT_OPEN_MONOPOLY "011"
-#define CLIENT_GET_CURRENT_POINTS "012"
-#define CLIENT_ACCEPT_TRADE "013"
-#define CLIENT_DECLINE_TRADE "014"
+#define CLIENT_OPEN_YEAR_OF_PLENTY "010" //%d%d, resource1, resource2 
+#define CLIENT_OPEN_MONOPOLY "011" //%d, resource
+#define CLIENT_GET_CURRENT_POINTS "012" 
+#define CLIENT_ACCEPT_TRADE "013" 
+#define CLIENT_DECLINE_TRADE "014" 
 //when 7 is rolled
-#define CLIENT_DISCARD_CARDS "024" 
+#define CLIENT_DISCARD_CARDS "024" //%d%s, len, resources (111) means 3 ore
 
-#define SERVER_START_TURN "015"
-#define SERVER_GIVE_DEV_CARD "016"
+#define SERVER_START_TURN "015" 
 #define SERVER_OFFER_TRADE "017"
 #define SERVER_FINAL_TRADE_POSITIVE "018"
 #define SERVER_FINAL_TRADE_NEGATIVE "019"
 #define SERVER_ACCEPT_OPERATION "020"
 #define SERVER_DECLINE_OPERATION "021" 
-#define SERVER_GIVE_CURRENT_POINTS "022"
+#define SERVER_GIVE_CURRENT_POINTS "022" //%d, points
 #define SERVER_GIVE_STOLEN_CARD "023"
 #define SERVER_GIVE_RESOURCES "025"
+#define SERVER_VICTORY "016" // %d, color
 
 #endif 
--- server/Server.c
+++ server/Server.c
@@ -14,17 +14,6 @@
 
 int main() {
     srand(time(NULL));
-    Board* board = createBoard();
-    int numOfPlayers;
-    while(true) {
-        numOfPlayers = getNumber("enter number of players (between 2 to 4): ");
-        if(numOfPlayers >= 2 && numOfPlayers <= 4) break;
-    }
-    Player** players = (Player**)malloc(sizeof(Player*)*numOfPlayers);
-
-
-    free(players);
-    destructBoard(board);
-    free(board);
+    //TODO create server client
     return 0;
 }
